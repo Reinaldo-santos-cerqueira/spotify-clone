0881b3583875c11672197bfe23e9e94d3aebeabb
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-19 13:52:43 -0300

    feat: config initial for react navigation

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {Splash} from './src/pages/splash/splash';
+
+function App(): JSX.Element {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
