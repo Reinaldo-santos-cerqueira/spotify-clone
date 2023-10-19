bf352f8923a5a900cd6c0ca5e646fb32c9c1027d
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-19 17:56:56 -0300

    feat: add property for box

--- src/pages/splash/splash.tsx
+++ src/pages/splash/splash.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import Box from '../../components/box/box';
+import {Box} from '../../components/box/box';
+import { Image } from 'react-native';
+import Logo from '../../assets/logo.png';
 
 export function Splash() {
 	return(
-		<Box>
-      
+		<Box
+			backgroundColor='background'
+			flex={1}
+		>
+			<Image source={Logo} style={{width: 200, height: 200}}/>
 		</Box>
 	);
 }
