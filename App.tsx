ef0b191c332e79fa928ee1eae8a28934012f0268
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-19 17:55:41 -0300

    feat: add theme provider

--- App.tsx
+++ App.tsx
@@ -2,16 +2,22 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {Splash} from './src/pages/splash/splash';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from './src/theme/theme';
 
 function App(): JSX.Element {
 	const Stack = createNativeStackNavigator();
 
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<ThemeProvider theme={theme}>
+
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</ThemeProvider>
+
 	);
 }
 
